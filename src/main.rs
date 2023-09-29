e1dc32cc32b51d2a2dbdd629dfc0b6afcd34fd40
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-09-30 08:56:26 +0900

    fix typo

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ async fn main() {
     let app = Router::new().route("/", get(root));
     let addr = SocketAddr::from((([127, 0, 0, 1]), 3000));
     axum::Server::bind(&addr)
-        .serve(app.info_make_service())
+        .serve(app.into_make_service())
         .await
         .unwrap();
 }
