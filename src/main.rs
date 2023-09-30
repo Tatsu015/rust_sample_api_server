286b7dfc6392381127347ea1208c0ca42202a0b4
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-09-30 09:59:02 +0900

    add user endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use axum::{routing::get, Router};
+use axum::{http::StatusCode, routing::get, Json, Router};
+use serde::Serialize;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/user", get(get_user));
+
     let addr = SocketAddr::from((([127, 0, 0, 1]), 3000));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -13,4 +17,19 @@ async fn main() {
 
 async fn root() -> &'static str {
     return "hello";
+}
+
+async fn get_user() -> (StatusCode, Json<User>) {
+    let user = User {
+        id: 1234,
+        name: String::from("TestUser"),
+    };
+
+    (StatusCode::ACCEPTED, Json(user))
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    name: String,
 }
